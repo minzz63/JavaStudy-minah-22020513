28cbbaef6d750d3956d9139e10917d217bb85f46
minzz63 <minzz63@naver.com>
2022-05-13 21:11:39 +0900

    프로젝트 첫 커밋. 변수와 상수 수업 코드

---
+++ JavaStudy_minah/src/j01_첫수업/HelloJava.java
@@ -0,0 +1,11 @@
+package j01_첫수업;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, java");
+		System.out.print(10 + 5);
+	}
+
+}
